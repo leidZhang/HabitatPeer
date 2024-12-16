396954f5c1b53b2ba5592f4b1701de4ea74b4018
leidZhang <haoz28889@gmail.com>
2024-12-16 11:34:31 -0500

    custom actions

--- launch_env.py
+++ launch_env.py
@@ -14,13 +14,13 @@ from habitat.core.env import Env
 from habitat.core.agent import Agent
 from habitat.core.simulator import Observations
 
-from scene.env import CustomEnv
+from scene.env import CustomEnv, CustomedChallenge
 from scene.agent import HabitatActuator
 from remote import ProviderPeer
 
 
 def start_habitat_env(queue_list: List[Queue]) -> None: # Only for test purposes
-    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]    
+    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]
     agent: HabitatActuator = HabitatActuator()
     for i, queue in enumerate(queue_list):
         agent.set_queue(queue_name=queue_names[i], queue=queue)
@@ -34,19 +34,19 @@ def start_habitat_env(queue_list: List[Queue]) -> None: # Only for test purposes
         if done:
             break
     print("Episode completed, closing environment...")
-     
+
 def start_habitat_benchmark(queue_list: List[Queue]) -> None: # Put the agent to the challenge
-    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]    
+    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]
     agent: HabitatActuator = HabitatActuator()
     for i, queue in enumerate(queue_list):
         agent.set_queue(queue_name=queue_names[i], queue=queue)
     os.environ["CHALLENGE_CONFIG_FILE"] = "configs/hm3d_test.yaml"
-    challenge: habitat.Challenge = habitat.Challenge(eval_remote=False)
+    challenge: habitat.Challenge = CustomedChallenge(eval_remote=False)
     challenge.submit(agent)
 
 
 def start_aiortc_client(queue_list: List[Queue]) -> None:
-    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]      
+    queue_names: list = ["depth", "rgb", "state", "action", "semantic"]
     with open("ip_configs.json", "r") as f:
         config: dict = json.load(f)
 
